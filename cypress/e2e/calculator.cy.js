05b632322d6b738d4c4f40c8e7d341ed0840784f
nurselaine <Elaine.t.huynh@gmail.com>
2023-03-06 10:16:38 -0800

    Created cypress test suites for volunteering, donating, and calculator user stories

---
+++ cypress/e2e/calculator.cy.js
@@ -0,0 +1,23 @@
+/// <reference types="cypress" />
+
+describe('Calculator User Stories', () => {
+  beforeEach(() => {
+    cy.visit("http://localhost:3000");
+  });
+
+  it('User can access calculator', () => {
+    cy.visit('https://example.cypress.io')
+  })
+
+  it('User with a misdemeanor that can be vacated', () => {
+    cy.visit('https://example.cypress.io')
+  })
+
+  it('User with a misdemeanor that cannot be vacated', () => {
+    cy.visit('https://example.cypress.io')
+  })
+
+  it('User looking for resources for jobs, housing, and education', () => {
+    cy.visit('https://example.cypress.io')
+  })
+})
---
+++ cypress/e2e/volunteer.cy.js
@@ -0,0 +1,21 @@
+describe('Volunteer User Stories', () => {
+  beforeEach(() => {
+    cy.visit("http://localhost:3000");
+  });
+
+  it('User can visit get-involved page', () => {
+    cy.visit('https://example.cypress.io')
+  })
+
+  it('User can learn about ways to contirbute and view open volunteer roles', () => {
+    cy.visit('https://example.cypress.io')
+  })
+
+  it('User can apply for open roles', () => {
+    cy.visit('https://example.cypress.io')
+  })
+
+  it('User can contact organization to ask questions about getting involved', () => {
+    cy.visit('https://example.cypress.io')
+  })
+})
